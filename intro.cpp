8127e9132b2a144a08d93ced038a2fcd0626281f
Ryantoaste <45082665+Ryantoaste@users.noreply.github.com>
2021-09-15 12:52:38 -0700

    Update intro.cpp

--- intro.cpp
+++ intro.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
-
+void other() {
+    cout << "it works";
+}
 int main(){
    /*int input = 0;
    char plus;
@@ -11,8 +15,23 @@ int main(){
    cout << plus << endl;
    cout << secint << endl;
    cout << "=" << endl << "21";
-   return 0;*/
-   float longput = 324.6674839;
-   cout.precision(11);
-   cout << longput;
+   cout.setf(ios::showpoint);
+   cout.setf(ios::fixed, ios::floatfield);
+   cout.precision(2);
+   float longput = 32423424.000;
+   cout << longput << endl;
+   cout.unsetf(ios::showpoint);
+   srand((unsigned)time(0));
+   int i;
+   i = (rand()%6)+1;
+   cout << i << endl;*/
+   int may = 0;
+   cin >> may;
+   if (may == 3)
+   {
+    cout << "no" << endl;
+   }
+   other();
+   return 0;
 }
+
