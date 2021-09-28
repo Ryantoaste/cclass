27e525605ad664442864535ac49f6e3e677e2045
Ryantoaste <45082665+Ryantoaste@users.noreply.github.com>
2021-09-28 14:40:30 -0700

    done!!
    I liked this project.

--- palindrome.cpp
+++ palindrome.cpp
@@ -71,20 +71,35 @@ int checkn(){
 }
 int palin(){
     int flag;
+    //halfs purpose is so if the user enters a palindrome with an odd number of characters, 
+    //it will check the letters past the middle letter, and before it, because otherwise,
+    //words like oko would not be counted as palindromes.
     lets -= half;
     for (int o = 0; o < (lets/2); o++){
+        //appends the first half of store to palf, because palf will be compared with
+        //the second half of store, or the palindrome.
         palf[o] = store[o];
     }
+    //countopalf makes sure the compared character from palf is increasing.
     int countopalf = 0;
     for (int u = ((lets + half)- 1); u > (lets / 2); u--){
+        //the condition makes sure that it starts comparing at the last character in store,
+        //then goes one lower every loop, and doesnt go below halfway through store.
         if (store[u] == palf[countopalf]){
+            //if it equals palf, it moves on to the next character of palf
             countopalf++;
         }else{
+            //if it does not equal palf, flag is set to one and the loop breaks,
+            //because it is not a palindrome.
             flag = 1;
             break;
         }
     }
     if (flag == 1){
+        //if the user does not enter a palindrome, (according to flag)
+        //all the arrays and variables are reset and the program goes back to the beginning
+        //function.
+        //it also tells them that is not a palindrome.
         cout << "that is not a palindrome" << endl;
         flag = 0;
         std::fill_n(store, 80, 0);
@@ -93,21 +108,32 @@ int palin(){
         lets = 0;
         main();
     }else{
+        //otherwise, the program says goodjob, tells them the palindrome they entered, 
         cout << "you entered " << store << " which is a palindrome! Great job!" << endl;
+        //and goes to the end function.
         end();
     }
     return 0;
 }
+//this function asks the user whether they want to restart or exit, and makes
+//sure they only enter restart or exit.
 int end(){
+    //string used for user input.
     string proposal;
     cout << "Good job! You made a palindrome!" << endl;
     cout << "Would you like to exit the program, or restart? (exit, restart)" << endl;
+    //gets user input.
     cin >> proposal;
+    //the below line makes the user input that was entered lowercase, to avoid 
+    //RESTART not being seen as restart or vice versa.
     transform(proposal.begin(), proposal.end(), proposal.begin(), :: tolower);
     if (proposal == "exit"){
+        //if the user wishes to exit, program says goodbye and exits the program.
         cout << "Ok goodbye." << endl;
         exit(0);
     }else if (proposal == "restart"){
+        //if the user wants to restart, the program resets all the arrays and
+        //variables, says Again!! and goes back to the beginning function.
         cout << "Again!!" << endl;
         std::fill_n(store, 80, 0);
         std::fill_n(palf, 40, 0);
@@ -116,6 +142,8 @@ int end(){
         cin.clear();
         main();
     }else{
+        //however, if the user does not enter either restart or exit, the program
+        //tells them they screwed up, and makes them enter it again.
         cout << "Only enter exit, or restart." << endl;
         end();
     }
