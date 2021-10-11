333badd46be2444f77849fae700928dfc80ff171
Ryantoaste <45082665+Ryantoaste@users.noreply.github.com>
2021-10-11 14:08:38 -0700

    second to last possible commit
    yep.

--- tictac.cpp
+++ tictac.cpp
@@ -6,7 +6,14 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
-int rndom;
+int y;
+int win;
+int xwins;
+int owins;
+int end();
+string victor;
+int victory();
+string rndom;
 int turn = 1;
 string playersnum;
 string player1;
@@ -42,19 +49,21 @@ int main(){
         cout << "only say 1 or two for number of players." << endl;
         main();
     }
+    mark();
     play();
     return 0;
 }
 int mark(){
     if (turn%2 == 1){
         wrote = player1;
+        victor = "Player 1";
     }else{
         wrote = player2;
+        victor = "Player 2";
     }
     return 0;
 }
 int play(){
-    mark();
     done = 0;
     string move;
     cout << "| " << places[0] << " | " << places[1] << " | " << places[2] << endl;
@@ -76,22 +85,84 @@ int play(){
             turn += 1;
         }
     }
-    cout << playersnum << endl;
     if (done == 0){
         cout << "Only type the numbers in the grid to make a move." << endl;
         play();
     }else if (playersnum == "1"){
+        victory();
+        mark();
         other();
+    }else{
+        victory();
+        mark();
+        play();
     }
     return 0;
 }
 int victory(){
-    
+    if (wrote == places[0] && wrote == places[4] && wrote == places[8]){
+        win = 1;
+    }else if (wrote == places[0] && wrote == places[3] && wrote == places[6]){
+       win = 1;
+    }else if (wrote == places[0] && wrote == places[1] && wrote == places[2]){
+        win = 1;
+    }else if (wrote == places[1] && wrote == places[4] && wrote == places[7]){
+        win = 1;
+    }else if (wrote == places[2] && wrote == places[4] && wrote == places[6]){
+        win = 1;
+    }else if (wrote == places[2] && wrote == places[5] && wrote == places[8]){
+        win = 1;
+    }else if (wrote == places[3] && wrote == places[4] && wrote == places[5]){
+        win = 1;
+    }else if (wrote == places [6] && wrote == places[7] && wrote == places[8]){
+        win = 1;
+    }
+    if (win == 1){
+        cout << victor << " has won the game! The game took " << turn << " tries!" << endl;
+        end();
+    }else if(turn == 9){
+        cout << "There is a tie between player 1 and 2. Noone wins." << endl;
+        victor = "";
+        end();
+    }
+    return 0;
+}
+int end(){
+    if (victor == "Player 1"){
+        owins++;
+    }else if (victor == "Player 2"){
+        xwins++;
+    }
+    cout << "o has won " << owins << " times and x has won " << xwins << " times." << endl;
+    cout << "would you like to exit or restart? ";
+    string finalDes;
+    cin >> finalDes;
+    if (finalDes == "exit") {
+        cout << "goodbye." << endl;
+        exit(0);
+    }else if(finalDes == "restart") {
+        cout << "Again!" << endl;
+        places == { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        takelist == { "", "", "", "", "", "", "", "", "" };
+        turn = 1;
+        win = 0;
+    }
     return 0;
 }
 int other(){
     srand((unsigned)time(0));
     rndom = (rand()%8)+0;
+    cout << rndom << endl;
+    for(int y; y < 9; y++){
+        if (takelist[y] == rndom){
+            y = y;
+            other();
+        }
+    }
+    places[y] = wrote;
+    turn += 1;
+    victory();
+    mark();
     play();
     return 0;
 }
